ffaca8de940b344be9190716fe7985b43e3cd504
Pradeep vemulakonda <pradeepvemulakonda@gmail.com>
2021-03-27 00:37:25 +1100

    added lowDB

--- app.js
+++ app.js
@@ -1,79 +1,86 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var yargs = require('yargs');
-var fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-var busboy = require('connect-busboy');
-var fs = require('fs-extra'); 
-const indexRouter = require('./routes/index');
-const setupRouter = require('./routes/setup');
-const X_CORRELATION_ID = 'x-correlation-id';
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const yargs = require('yargs')
+const { v4: uuidv4 } = require('uuid')
+const busboy = require('connect-busboy')
+const fs = require('fs-extra')
+const low = require('lowdb')
+const SwaggerParser = require('@apidevtools/swagger-parser')
+const FileSync = require('lowdb/adapters/FileSync')
+const indexRouter = require('./routes/index')
+const setupRouter = require('./routes/setup')
+const X_CORRELATION_ID = 'x-correlation-id'
 
 const options = yargs
   .usage('Usage: -b < /to/mock/file/base/directory')
   .option('b', { alias: 'basepath', description: 'path to base directory where mock project files are present', demandOption: false })
   .option('p', { alias: 'port', description: 'server port', demandOption: false })
-  .argv;
+  .argv
 
-const basePath =   options.basepath || path.join(__dirname, 'public');
+const basePath = options.basepath || path.join(__dirname, 'public')
 
-function validatePath(path) {
-  const isPathValid = fs.existsSync(path);
+const adapter = new FileSync('database/db.json')
+const db = low(adapter)
+db.defaults({ apis: [] })
+  .write()
+
+function validatePath (path) {
+  const isPathValid = fs.existsSync(path)
   if (!isPathValid) {
-    throw new Error(`Path not found: ${path}`);
+    throw new Error(`Path not found: ${path}`)
   }
-  console.log(`Setting the mock base directory to ${path}`);
+  console.log(`Setting the mock base directory to ${path}`)
 }
 
-function validatePathNotEmpty(path) {
-  const isEmptyDirectory = fs.readdirSync(path).length === 0;
+function validatePathNotEmpty (path) {
+  const isEmptyDirectory = fs.readdirSync(path).length === 0
   if (isEmptyDirectory) {
-    throw new Error(`No mocks found at : ${path}`);
+    throw new Error(`No mocks found at : ${path}`)
   }
-  console.log(`Serving the mocks from: ${path}`);
+  console.log(`Serving the mocks from: ${path}`)
 }
 
-function parseCorrelationId(req) {
-  const correlationId = req.get(X_CORRELATION_ID);
-  const correlationElements = correlationId.split('-');
-  const requestedResponseCode = parseInt(correlationElements[1]);
-  const responseTimeDelay = parseInt(correlationElements[2]) || 0;
-  return { requestedResponseCode, responseTimeDelay };
+function parseCorrelationId (req) {
+  const correlationId = req.get(X_CORRELATION_ID)
+  const correlationElements = correlationId.split('-')
+  const requestedResponseCode = parseInt(correlationElements[1])
+  const responseTimeDelay = parseInt(correlationElements[2]) || 0
+  return { requestedResponseCode, responseTimeDelay }
 }
 
 // the url should not contain the response delay provided as part of correlation id
-function updateUrl(req) {
-  const correlationId = req.get(X_CORRELATION_ID);
-  const correlationElements = correlationId.split('-');
-  correlationElements.length > 2 && correlationElements.pop();
-  const fileName = correlationElements.join('-');
-  req.url = path.join(basePath, req.originalUrl, fileName + '.json');
-  req.method = 'GET';
+function updateUrl (req) {
+  const correlationId = req.get(X_CORRELATION_ID)
+  const correlationElements = correlationId.split('-')
+  correlationElements.length > 2 && correlationElements.pop()
+  const fileName = correlationElements.join('-')
+  req.url = path.join(basePath, req.originalUrl, fileName + '.json')
+  req.method = 'GET'
 }
 
-validatePath(basePath);
-validatePathNotEmpty(basePath);
+validatePath(basePath)
+validatePathNotEmpty(basePath)
 
-var app = express();
-app.port = options.port;
-app.set('basepath', basePath);
+const app = express()
+app.port = options.port
+app.set('basepath', basePath)
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(busboy());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+app.use(busboy())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', indexRouter);
-app.use('/setup', setupRouter);
+app.use('/', indexRouter)
+app.use('/setup', setupRouter)
 
 app.route('/upload')
   .post(function (req, res, next) {
@@ -90,61 +97,70 @@ app.route('/upload')
       }
       const fstream = fs.createWriteStream(finalFileName)
       file.pipe(fstream)
-      fstream.on('close', function () {
+      fstream.on('close', async function () {
         console.log('Upload Finished of ' + filename)
+        const api = await parseSwagger(finalFileName)
+        console.info('api', api)
+        db.get('apis')
+          .push({ name: api.basePath, paths: api.paths })
+          .write()
+        // Set some defaults (required if your JSON file is empty)
         res.redirect('/setup') // where to go next
       })
     })
   })
 
+const parseSwagger = async (swagger) => {
+  return await SwaggerParser.validate(swagger)
+}
 
 app.use(function (req, res, next) {
-  var correlationId = req.header(X_CORRELATION_ID);
+  const correlationId = req.header(X_CORRELATION_ID)
   if (correlationId) {
-    updateUrl(req);
-    next();
+    updateUrl(req)
+    next()
   } else {
-    return next(createError(400, `Invalid ${X_CORRELATION_ID}`));
-  } 
-});
+    return next(createError(400, `Invalid ${X_CORRELATION_ID}`))
+  }
+})
 
 app.use(function (req, res, next) {
-  const { requestedResponseCode, responseTimeDelay } = parseCorrelationId(req);
-  const filePath = path.join(req.url);
-  console.log(`Mock file being processed is: ${filePath}`);
+  const { requestedResponseCode, responseTimeDelay } = parseCorrelationId(req)
+  const filePath = path.join(req.url)
+  console.log(`Mock file being processed is: ${filePath}`)
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       return next(createError(400, err))
     }
-    console.log(`requested response code : ${requestedResponseCode} and response delay is ${responseTimeDelay}`);
+    console.log(`requested response code : ${requestedResponseCode} and response delay is ${responseTimeDelay}`)
     if (requestedResponseCode >= 200 && requestedResponseCode < 600) {
       setTimeout(() => {
-        res.status(requestedResponseCode);
-        res.json(JSON.parse(data));
-      }, responseTimeDelay);
+        res.status(requestedResponseCode)
+        res.json(JSON.parse(data))
+      }, responseTimeDelay)
     } else {
-      return next(createError(500, `Inalid http status code ${requestedResponseCode}`));
+      return next(createError(500, `Inalid http status code ${requestedResponseCode}`))
     }
-  });
-});
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  var errorMessage = err.message || 'error in processing request';
+  const errorMessage = err.message || 'error in processing request'
   // render the error page
   res.json({
     message: errorMessage,
     error: '1001'
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
